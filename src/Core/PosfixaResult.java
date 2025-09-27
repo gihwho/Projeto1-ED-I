3fbcbf835ef436ab419a4e68a72869b648a23de9
Giovanna Borges <giovannaborgesmassi@gmail.com>
2025-09-27 08:35:15 -0300

    add TODO

--- src/Core/PosfixaResult.java
+++ src/Core/PosfixaResult.java
@@ -11,4 +11,12 @@ package src.Core;
 
 public class PosfixaResult {
     
+    private static double aplica(char op, double b, double a) throws Exception {
+        //TODO : aplicar operador binario em dois operandos do topo da pilha
+    }
+
+    public static double avaliar(String posfixa, Simbolos tab) throws Exception {
+        //TODO : avaliar a expressão posfixa
+    }
+
 }
